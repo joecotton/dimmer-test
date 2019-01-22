29dc60da385fa33aefb931b0d848efcffe584a00
Joseph Cotton <joe@lightningflash.net>
2019-01-21 20:55:43 -0600

    ch...ch...ch...changes.

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,18 @@
 #define LED_PIN 10
 #define REPORT_INTERVAL 500
 
-char inputBuffer[MAX_INPUT + 1]; // Handles up to 90 bytes in a c-style string, with a null character termination.
+#define DIMMER_MIN 0
+#define DIMMER_MAX_DEFAULT 100
+#define DIMMER_DELAY_MS 100
+
+uint16_t dimmer_max = DIMMER_MAX_DEFAULT;
+
+// char inputBuffer[MAX_INPUT + 1]; // Handles up to 90 bytes in a c-style string, with a null character termination.
 
 void setup()
 {
   Serial.begin(115200);  // initialization
-  inputBuffer[0] = '\0'; //Initialize string to emtpy.
+  // inputBuffer[0] = '\0'; //Initialize string to emtpy.
 
   Timer1.initialize(255);
   Timer1.pwm(LED_PIN, 0);
@@ -19,77 +25,43 @@ void setup()
   // pinMode(AINPUT, INPUT);
 
   Serial.println("Begin:");
-
-  // Setting 	Divisor 	Frequency
-  // 0x01 	 	1  		31372.55
-  // 0x02 	 	8 	 	3921.16
-  // 0x03  		32  		980.39
-  // 0x04 	 	64 	 	490.20   <--DEFAULT
-  // 0x05 	 	128  		245.10
-  // 0x06  		256  		122.55
-  // 0x07 	 	1024  		30.64
-
-  // TCCR2B = (TCCR2B & 0b11111000) | <setting>;
-  // TCCR2B = (TCCR2B & 0b11111000) | 0x02; // 3.9kHz frequency
-
-  // All frequencies are in Hz and assume a 16000000 Hz system clock.
 }
 
 void loop()
 {
-  static uint8_t newPWM;
   static uint16_t pwmVal;
-  static uint16_t a_in;
-  static uint16_t lastRead;
+  static uint32_t lastRead;
+  static uint32_t lastDim;
+  static uint8_t dir;
 
-  a_in = analogRead(AINPUT);
-  Timer1.setPwmDuty(LED_PIN, a_in);
+  dimmer_max = analogRead(AINPUT);
 
-  if (millis() - lastRead > REPORT_INTERVAL) {
-    lastRead = millis();
-    Serial.println(a_in);
-  }
-
-  // if (newPWM) {
-  //   // analogWrite(LED_PIN, pwmVal);
-  //   // TCCR2B = (TCCR2B & 0b11111000) | 0x02; // 3.9kHz frequency
-  //   Timer1.setPwmDuty(LED_PIN, pwmVal);
-  //   newPWM = 0x00;
-  // }
-
-  if (Serial.available() > 0)
+  if (millis() - lastRead > REPORT_INTERVAL)
   {
-    char input = Serial.read();
-    static int s_len; // static variables default to 0
-    if (s_len >= MAX_INPUT)
-    {
-      // Have received already the maximum number of characters
-      // Ignore all new input until line termination occurs
-    }
-    else if (input != '\n' && input != '\r')
-    {
-      inputBuffer[s_len++] = input;
-    }
-    else
-    {
-      // Have received a LF or CR character
-
-      // INSERT YOUR CODE HERE TO PROCESS THE RECEIVED DATA //
-      // YOU COULD COPY TO A NEW VARIABLE WITH strncpy() OR //
-      // SET A FLAG TO SAY TO START SOME OTHER TASK         //
-      Serial.print("RECEIVED MSG: ");
-      Serial.println(inputBuffer);
-      newPWM = 0x01;
-      pwmVal = atoi(inputBuffer);
-
-      memset(inputBuffer, 0, sizeof(inputBuffer));
-      s_len = 0; // Reset input buffer here if you
-                 // have already copied the data.
-                 // If you don't reset here, then
-                 // you can't start receiving more
-                 // serial port data. This is your
-                 // 'software' serial buffer, contrast
-                 // with the hardware serial buffer.
-    }
+    lastRead = millis();
+    Serial.print("pwmVal:");
+    Serial.print(pwmVal);
+    Serial.print("/");
+    Serial.println(dimmer_max);
+    Serial.print("dir:");
+    Serial.println(dir);
+    Serial.print("millis():");
+    Serial.println(millis());
   }
-}
+
+  if (millis() - lastDim > DIMMER_DELAY_MS)
+  {
+    lastDim = millis();
+    if ( pwmVal==0) {
+      dir = 1;
+    } else if (pwmVal >= dimmer_max) {
+      dir = 0;
+    }
+    if (dir) {
+      pwmVal++;
+    } else {
+      pwmVal--;
+    }
+    Timer1.setPwmDuty(LED_PIN, pwmVal);
+  }
+ }
